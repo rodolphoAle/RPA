a09569ec66276f00ef78f0682eb0cd5996396343
seraeac <rodolphosilva.ale@gmail.com>
2024-11-23 08:20:39 -0300

    feat: commit, craft my first RPA, apenas para fins de teste

---
+++ FistRpa/FirstRpa.py
@@ -0,0 +1,26 @@
+import pyautogui as mouse
+import pyautogui as keyboard
+
+keyboard.sleep(1)
+
+# movenco o cursor ate o iniciar
+mouse.moveTo(145,1055)
+mouse.click()
+keyboard.sleep(1)
+
+#digitando e abrindo o app
+keyboard.typewrite('google')
+keyboard.sleep(1)
+mouse.moveTo(278,455)
+keyboard.sleep(1)
+mouse.click()
+keyboard.sleep(3)
+
+#digitando o input kkkkk
+keyboard.hotkey('ctrl','shitf','n')
+keyboard.sleep(3)
+keyboard.typewrite('xvideos.com')
+keyboard.hotkey('enter')
+
+print('Congratularion your PUNHETEIRO')
+
